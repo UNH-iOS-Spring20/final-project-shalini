8cbeaa856f8c386a095fed9a5ddf44969ae3787c
Shalini Garikapati <shalugarikapaty96@gmail.com>
2020-04-30 21:55:53 -0400

    Multiple selection of rows

---
+++ final-project-shalini/final-project-shalini/View/MultipleSelectionRow.swift
@@ -0,0 +1,21 @@
+//
+//  MultipleSelectionRow.swift
+//  final-project-shalini
+//
+//  Created by Shalu Garikapaty on 4/30/20.
+//  Copyright © 2020 Shalini Garikapaty. All rights reserved.
+//
+
+import SwiftUI
+
+struct MultipleSelectionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MultipleSelectionRow_Previews: PreviewProvider {
+    static var previews: some View {
+        MultipleSelectionRow()
+    }
+}
