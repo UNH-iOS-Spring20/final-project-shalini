294e6cb7b0597d62b376bfc80332c24d9371e698
Shalini Garikapati <shalugarikapaty96@gmail.com>
2020-05-10 15:10:35 -0400

    Delete Symptom.swift

--- final-project-shalini/final-project-shalini/View/Model/Symptom.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Symptom.swift
-//  final-project-shalini
-//
-//  Created by Shalu Garikapaty on 4/13/20.
-//  Copyright © 2020 Shalini Garikapaty. All rights reserved.
-//
-
-import Foundation
