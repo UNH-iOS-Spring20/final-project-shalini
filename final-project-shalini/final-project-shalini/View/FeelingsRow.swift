6b85c1320f8ca7e8c5332af6c6c79128d1c71776
Shalini Garikapati <shalugarikapaty96@gmail.com>
2020-05-12 15:52:42 -0400

    Delete FeelingsRow.swift

--- final-project-shalini/final-project-shalini/View/FeelingsRow.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FeelingsRow.swift
-//  final-project-shalini
-//
-//  Created by Shalu Garikapaty on 3/22/20.
-//  Copyright © 2020 Shalini Garikapaty. All rights reserved.
-//
-
-import Foundation
