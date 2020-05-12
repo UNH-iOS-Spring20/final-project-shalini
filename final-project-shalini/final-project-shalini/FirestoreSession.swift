86ee69f7cc4b946c87f6a2657e8c7f15b7c9248e
Shalini Garikapati <shalugarikapaty96@gmail.com>
2020-05-12 14:18:52 -0400

    Delete FirestoreSession.swift

--- final-project-shalini/final-project-shalini/FirestoreSession.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FirestoreSession.swift
-//  final-project-shalini
-//
-//  Created by Shalu Garikapaty on 4/14/20.
-//  Copyright © 2020 Shalini Garikapaty. All rights reserved.
-//
-
-import Foundation
